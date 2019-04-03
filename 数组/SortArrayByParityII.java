7587d105b2d86ba2ef5cc0a02f951f4e28eb02b7
杜凯超 <2822365215@qq.com>
2019-04-03 23:34:11 +0800

    奇偶数组二

---
+++ 数组/SortArrayByParityII.java
@@ -0,0 +1,29 @@
+package dkc_0403;
+/*
+ 	给定一个非负整数数组 A， A 中一半整数是奇数，一半整数是偶数。
+对数组进行排序，以便当 A[i] 为奇数时，i 也是奇数；当 A[i] 为偶数时， i 也是偶数。
+你可以返回任何满足上述条件的数组作为答案。
+
+	示例：
+	输入：[4,2,5,7]
+	输出：[4,5,2,7]
+	解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ */
+public class SortArrayByParityII {
+	public int[] sortArrayByParityII(int[] arr) {
+        int len = arr.length;
+        int[] array = new int[len];
+        int head = 0;
+        int next = 1;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]%2 == 0){
+                array[head] = arr[i];
+                head = head+2;
+            }else{
+                array[next] = arr[i];
+                next = next+2;
+            }
+        }
+        return array;
+    }
+}
